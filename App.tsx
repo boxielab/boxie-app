076bde1e964ac1f62bde1f345b09b59640d44203
progvar <prog.varadi@gmail.com>
2021-02-12 01:16:13 +0100

    merge: resolve conflicts with develop

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import { LoginScreen }  from './src/screens'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   return (
@@ -12,12 +21,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
